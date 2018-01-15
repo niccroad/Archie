67dd3b83b585eefba5de92f16de643b4828fde58
Nicolas Croad <nic.croad@metservice.com>
2018-01-15 17:10:35 +1300

    Don't allow underbar as a wildcard input pattern as this messes with the more common case of a name ending in _p.h or similar.

--- archie/projectmodel/entities/ProjectLayout.py
+++ archie/projectmodel/entities/ProjectLayout.py
@@ -101,17 +101,16 @@ class ProjectLayout(object):
         self.tier_patterns[tier].append(tier_pattern)
 
     def _convertWildcardsToMatcher(self, pattern):
-    	replace_dict = {'.':'\.',
-    		            '_':'.',
-    		            '**/':'[\w\s/]*',
-    		            '**':'[\w\s/]+',
-    	                '*':'[\w\s]+'}
-    	replace_keys = replace_dict.keys()
-    	replace_keys = map(lambda x: re.escape(x), replace_keys)
-    	replace_pattern = re.compile('|'.join(replace_keys))
-    	pattern = (replace_pattern.sub(lambda x:replace_dict[x.group()],
-    		                           pattern)
-    		    + '$')
+        replace_dict = {'.':'\.',
+                        '**/':'[\w\s/.]*',
+                        '**':'[\w\s/.]+',
+                        '*':'[\w\s.]+'}
+        replace_keys = replace_dict.keys()
+        replace_keys = map(lambda x: re.escape(x), replace_keys)
+        replace_pattern = re.compile('|'.join(replace_keys))
+        pattern = (replace_pattern.sub(lambda x:replace_dict[x.group()],
+                                       pattern)
+                + '$')
         matcher = re.compile(pattern)
         return matcher
 
